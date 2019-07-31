6784b53f2d9c613052bc420314c7aa701f804b36
Dmitriy Simontsev <dimsim@mail.ru>
2019-07-31 13:29:48 +0700

    Refactoring. Some dead code removed. Breadcrumb added.

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ui breadcrumb">
+        <div v-for="(item, index) in path" :key="index" class="section" :class="{ active: isActive(index) }">
+            {{item}}
+            <i v-if="index !== path.length - 1 " class="right chevron icon divider"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumb',
+  props: {
+    path: Array,
+    default: []
+  },
+  methods: {
+    isActive: function (i) {
+      return i === this.path.length - 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .active {
+        color: #1e70bf;
+    }
+</style>
--- src/parent/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by ayou on 17/7/21.
- */
-exports.VueTreeList = require('./VueTreeList.vue')
-exports.TreeNode = require('./Tree.js').TreeNode
-exports.Tree = require('./Tree.js').Tree
-// exports.Record = require('./Tree.js').Record
